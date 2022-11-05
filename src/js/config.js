86d87682d4ec508c406bbf9a7f44283f7bace649
SlavenSaric <slavendownload@gmail.com>
2022-11-05 19:56:19 +0100

    added new feature

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '2b453973-cfa6-4e88-bf42-9d2c38082749'
 export const MODAL_CLOSE_SEC = 2.5
+const AI = 'this is AI'
